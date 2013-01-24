7c193a466db77a434222ee03ee732e85c648d950
Thiago Prado <coelhudo@gmail.com>
2013-01-24 12:50:40 -0200

    Now with average damping

--- 1_36.scm
+++ 1_36.scm
@@ -12,3 +12,6 @@
   (try first-guess))
 
 (fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0) ;4.555540912917957 - 34 steps without average damping
+
+(define (average a b) (/ (+ a b) 2.))
+(fixed-point (lambda (x) (average (/ (log 1000) (log x)) x)) 2.0) ;4.5555465521473675 - 9 steps with average damping
