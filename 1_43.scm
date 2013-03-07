b2345b3babfdce137eb87ef68d98766b89d0cad5
Thiago Prado <coelhudo@gmail.com>
2013-03-07 12:47:39 -0300

    Create 1_43.scm

---
+++ 1_43.scm
@@ -0,0 +1,12 @@
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeat f n)
+  (if (= n 1)
+      f
+      (compose f (repeat f (- n 1)))))
+
+(define (square x)
+  (* x x))
+
+((repeat square 2) 5); 625
