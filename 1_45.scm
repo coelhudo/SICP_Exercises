afd68a165c815aa3c521f49aed1dae8b80f7facd
Thiago Prado <coelhudo@gmail.com>
2013-03-14 12:49:16 -0300

    Update 1_45.scm
    some experiments made, trying to define n-root function. Not working yet.

--- 1_45.scm
+++ 1_45.scm
@@ -19,3 +19,45 @@
     guess
 	  (try next))))
   (try first-guess))
+
+(define (square-root x)
+  (fixed-point (average-damp (lambda (y) (/ x y))) 1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (* y y)))) 1.0))
+
+(define (fourth-root x)
+  (fixed-point (average-damp (average-damp (lambda (y) (/ x (* y y y))))) 1.0))
+
+(define (fifth-root x)
+  (fixed-point (average-damp (average-damp (lambda (y) (/ x (* y y y y))))) 1.0))
+
+(define (sixth-root x)
+  (fixed-point (average-damp (average-damp (lambda (y) (/ x (* y y y y y))))) 1.0))
+
+(define (seventh-root x)
+  (fixed-point (average-damp (average-damp (lambda (y) (/ x (* y y y y y y))))) 1.0))
+
+(define (pow x n)
+  (* x (if (< n 3)
+	   x
+	   (pow x (- n 1)))))
+
+(define (n-root x n)
+  (if (< n 4)
+      (fixed-point (average-damp (lambda (y) (/ x (pow y (- n 1))))) 1.0)
+      (fixed-point (average-damp (average-damp (lambda (y) (/ x (pow y (- n 1))))) 1.0))))
+
+(square-root 4)
+(cube-root 8)
+(fourth-root 16)
+(fifth-root 32)
+(sixth-root 64)
+(seventh-root 128)
+
+(n-root 4 2)
+(n-root 8 3)
+(n-root 16 4)
+(n-root 32 5)
+(n-root 64 6)
+(n-root 128 7)
