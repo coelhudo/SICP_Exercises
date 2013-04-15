624d804ae42485c87a739fddb13d76734bdf45da
Thiago Prado <coelhudo@gmail.com>
2013-04-15 20:04:22 -0300

    Initial commit, not working

---
+++ ch2_01.scm
@@ -0,0 +1,29 @@
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (gcd a b)
+  (newline)
+  (display a)
+  (display " ")
+  (display b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (and (< g 0) (< d 0))
+  (cons (/ n (* g -1)) (/ d (* g -1)))
+	(cons (/ n g) (/ d g)))))
+
+(print-rat (make-rat 10 20))
+(print-rat (make-rat -10 20))
+(print-rat (make-rat -10 -20))
+(print-rat (make-rat 10 -20))
