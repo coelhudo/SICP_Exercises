312b3b49a68f2bff51108b1100b2b42aa7fd2424
Thiago Prado <coelhudo@gmail.com>
2013-11-04 03:03:46 -0200

    Create ch2_05.scm

---
+++ chapter_02/ch2_05.scm
@@ -0,0 +1,32 @@
+(define (logb n base)
+  (ceiling (/ (log n) (log base))))
+
+(define (find-value value n)
+  (if (= (remainder value n) 0)
+      (find-value (/ value n) n)
+      value))
+ 
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car z)
+  (if (= z 1)
+      0
+      (logb (find-value z 3) 2)
+      ))
+
+(define (cdr z)
+  (if (= z 1) 0
+      (logb (find-value z 2) 3)
+      ))
+
+(car (cons 0 0)) ;0
+(car (cons 0 1)) ;0
+(car (cons 1 0)) ;1
+(car (cons 1 1)) ;1
+(cdr (cons 0 0)) ;0
+(cdr (cons 0 1)) ;1
+(cdr (cons 1 0)) ;0
+(cdr (cons 1 1)) ;1
+(cdr (cons 23 24)) ;24
+(car (cons 23 24)) ;23
